3c57972ab7ababf2e6418c537be5885d4f31cabe
Bradley Adams <brad@bradleyadams.com>
2019-01-26 21:47:56 -0800

    Audio

---
+++ DogGroomer/Assets/Scripts/Fan.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Fan : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _fanSpeed;
+
+	private void Update()
+    {
+        transform.Rotate(_fanSpeed);
+	}
+}
